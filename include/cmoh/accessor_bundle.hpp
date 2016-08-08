76f9683f7256cd1609d4e9f7f4df1d7269fc7804
Julian Ganz <neither@nut.email>
2016-08-08 13:39:31 +0200

    Use `std::forward()` for the accessor bundle factory

--- include/cmoh/accessor_bundle.hpp
+++ include/cmoh/accessor_bundle.hpp
@@ -185,7 +185,7 @@ accessor_bundle<Accessors...>
 bundle(
     Accessors... accessors ///< accessors to bundle
 ) {
-    return accessor_bundle<Accessors...>(accessors...);
+    return accessor_bundle<Accessors...>(std::forward<Accessors>(accessors)...);
 }
 
 
